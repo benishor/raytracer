ba8288fd2710f39f2e56b826b9c61273f3a909a0
Adrian Scripca <benishor@gmail.com>
2019-02-27 13:08:24 +0200

    Added color operations

--- src/main/java/ro/scene/hq/raytracer/core/Tuple.java
+++ src/main/java/ro/scene/hq/raytracer/core/Tuple.java
@@ -3,6 +3,7 @@ package ro.scene.hq.raytracer.core;
 import java.util.Objects;
 
 public class Tuple {
+    private static final double EPSILON = 0.0001;
     public double x;
     public double y;
     public double z;
@@ -29,6 +30,10 @@ public class Tuple {
 
     public static Tuple vector(double x, double y, double z) {
         return new Tuple(x, y, z, 0);
+    }
+
+    public static Tuple color(double r, double g, double b) {
+        return new Tuple(r, g, b, 0);
     }
 
     public Tuple add(Tuple other) {
@@ -95,6 +100,19 @@ public class Tuple {
                 a.y * b.z - a.z * b.y,
                 a.z * b.x - a.x * b.z,
                 a.x * b.y - a.y * b.x
+        );
+    }
+
+    public static boolean areEqual(double a, double b) {
+        return Math.abs(a - b) < EPSILON;
+    }
+
+    public Tuple mul(Tuple other) {
+        return new Tuple(
+                x * other.x,
+                y * other.y,
+                z * other.z,
+                w * other.w
         );
     }
 }
--- src/test/java/ro/scene/hq/raytracer/core/TupleTest.java
+++ src/test/java/ro/scene/hq/raytracer/core/TupleTest.java
@@ -2,6 +2,7 @@ package ro.scene.hq.raytracer.core;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -117,7 +118,8 @@ public class TupleTest {
         assertThat(Tuple.mag(vector(1, 0, 0)), is(equalTo(1.0)));
         assertThat(Tuple.mag(vector(0, 1, 0)), is(equalTo(1.0)));
         assertThat(Tuple.mag(vector(0, 0, 1)), is(equalTo(1.0)));
-        assertThat(Tuple.mag(vector(1, 2, 3)), is(equalTo(Math.sqrt(14))));
+
+        assertTrue(areEqual(Tuple.mag(vector(1, 2, 3)), 3.7416));
     }
 
     @Test
@@ -150,4 +152,50 @@ public class TupleTest {
         assertThat(cross(a, b), is(equalTo(vector(-1, 2, -1))));
         assertThat(cross(b, a), is(equalTo(vector(1, -2, 1))));
     }
+
+    @Test
+    public void color_isATuple() {
+        Tuple c = color(1, 2, 3);
+
+        assertThat(c.x, is(equalTo(1.0)));
+        assertThat(c.y, is(equalTo(2.0)));
+        assertThat(c.z, is(equalTo(3.0)));
+    }
+
+    @Test
+    public void addingColors() {
+        Tuple c1 = color(0.9, 0.6, 0.75);
+        Tuple c2 = color(0.7, 0.1, 0.25);
+
+        assertThat(c1.add(c2), is(equalTo(color(1.6, 0.7, 1.0))));
+    }
+
+    @Test
+    public void subtractingColors() {
+        Tuple c1 = color(0.9, 0.6, 0.75);
+        Tuple c2 = color(0.7, 0.1, 0.25);
+
+        assertTupleEquals(c1.sub(c2), color(0.2, 0.5, 0.5));
+    }
+
+    @Test
+    public void multiplyingColorByScalar() {
+        Tuple c = color(0.2, 0.3, 0.4).mul(2);
+        assertTupleEquals(c, color(0.4, 0.6, 0.8));
+    }
+
+    @Test
+    public void multiplyingColors() {
+        Tuple c = color(1, 0.2, 0.4).mul(color(0.9, 1, 0.1));
+        assertTupleEquals(c, color(0.9, 0.2, 0.04));
+    }
+
+    private void assertTupleEquals(Tuple a, Tuple b) {
+        assertThat(areEqual(a.x, b.x), is(true));
+        assertThat(areEqual(a.y, b.y), is(true));
+        assertThat(areEqual(a.z, b.z), is(true));
+        assertThat(areEqual(a.w, b.w), is(true));
+    }
+
+
 }
